c41bdae109d05debf9fc3b10b2cf977e40eef7a8
Damilare <damilareakinfenwa1@gmail.com>
2023-07-15 16:35:13 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ * Return: 0 (Success)
+ */
+
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
